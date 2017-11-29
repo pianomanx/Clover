660e391637e21492d3e99ce3e3bd89934a7f7b85
RehabMan <racerrehabman@gmail.com>
2017-11-29 12:58:36 -0800

    restore option to boot without caches, even for 10.10 and later (OMFG\!)

--- rEFIt_UEFI/entry_scan/loader.c
+++ rEFIt_UEFI/entry_scan/loader.c
@@ -683,7 +683,7 @@ STATIC VOID AddDefaultMenu(IN LOADER_ENTRY *Entry)
   UINT64            VolumeSize;
   EFI_GUID          *Guid = NULL;
   BOOLEAN           KernelIs64BitOnly;
-  UINT64            os_version = AsciiOSVersionToUint64(Entry->OSVersion);
+//  UINT64            os_version = AsciiOSVersionToUint64(Entry->OSVersion);
 
   if (Entry == NULL) {
     return;
@@ -761,9 +761,10 @@ STATIC VOID AddDefaultMenu(IN LOADER_ENTRY *Entry)
     }
     AddMenuCheck(SubScreen, "Verbose (-v)",                               OPT_VERBOSE, 68);
     // No Caches option works on 10.6/10.7/10.8/10.9
-    if (os_version < AsciiOSVersionToUint64("10.10")) {
+    // RehabMan: it is also useful on 10.10/10.11/10.12/10.13
+    //if (os_version < AsciiOSVersionToUint64("10.10")) {
       AddMenuCheck(SubScreen, "Without caches (-f)",                        OPT_NOCACHES, 68);
-    }
+    //}
     AddMenuCheck(SubScreen, "Single User (-s)",                           OPT_SINGLE_USER, 68);
     AddMenuCheck(SubScreen, "Safe Mode (-x)",                             OPT_SAFE, 68);
     AddMenuCheck(SubScreen, "Disable KASLR (slide=0)",                    OPT_SLIDE, 68);
